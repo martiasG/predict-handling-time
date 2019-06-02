26f11492533a42448d69ae3bf9cb14ed81b602a9
martiasG <matias.mart.gonzalez@gmail.com>
2019-06-02 16:12:54 -0300

    model changes parameters

--- api_methods/model_actions.py
+++ api_methods/model_actions.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 from flask import make_response, abort
 import json
-from inference_model import model
+from model.inference_model import model
 import numpy as np
 from keras.utils import to_categorical
 
-def predict(handling_hour, created_hour, zip_code, order_cost):
-    prediction = int(model.predict(verbose=1, x=np.array([[created_hour, handling_hour, int(zip_code), order_cost]]))[0][0])
+def predict(handling_hour, created_hour, order_cost):
+    prediction = int(model.predict(verbose=1, x=np.array([[created_hour, handling_hour, order_cost]]))[0][0])
     return make_response(json.dumps({"ht_real" : prediction}))
---
+++ model/inference_model.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd 
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import keras as k
+from keras import layers
+from keras.layers import Input, Dense, Activation, BatchNormalization, Dropout
+from keras.models import Model, Sequential
+from keras.utils import to_categorical
+from keras.callbacks import TensorBoard
+
+def load_model():
+	model = Sequential()
+	model.add(Dense(2048, input_dim=3, activation='relu'))
+	model.add(Dense(1024, activation='relu'))
+	model.add(Dense(512, activation='relu'))
+	model.add(Dense(256, activation='relu'))
+	model.add(Dense(128, activation='relu'))
+	model.add(Dense(64, activation='relu'))
+	model.add(Dense(32, activation='relu'))
+	model.add(Dense(16, activation='relu'))
+	model.add(Dense(8, activation='relu'))
+	model.add(Dense(4, activation='relu'))
+	model.add(Dense(1, activation='relu'))
+
+	model.load_weights('./model/params_model_prod.h5')
+
+	return model
+
+model = load_model()
